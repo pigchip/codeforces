9f98137deee12e962794fa557662621aaf3ace79
pigchip <guzmancruzandresmiguel@gmail.com>
2023-09-19 21:26:02 -0600

    Problem div2 230A

---
+++ solutions/div2/230A/README.md
@@ -0,0 +1,6 @@
+# Problem 230A
+
+Problem description.
+
+[Link to Codeforces](https://codeforces.com/problemset/problem/div2/230A)
+
---
+++ solutions/div2/230A/main.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int s, n;
+    cin >> s >> n;
+
+    vector<pair<int, int>> dragons(n);
+
+    for (int i = 0; i < n; i++)
+        cin >> dragons[i].first >> dragons[i].second;
+
+    sort(dragons.begin(), dragons.end());
+
+    bool winning = true;
+
+    for (const auto& dragon : dragons) {
+        if (s <= dragon.first) {
+            winning = false;
+            break; 
+        } else {
+            s += dragon.second;
+        }
+    }
+
+    cout << (winning ? "YES" : "NO");
+
+    return 0;
+}
